787d03738aee6a10019345f73f6906785bf2252f
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-04-29 17:05:55 +0200

    all done with style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create]
+
+  resources :bookmarks, only: [:destroy]
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
