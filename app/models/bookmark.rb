0d941fd9138b4f091fffad6fc4a530fedc8c9eb9
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-04-29 11:55:45 +0200

    models validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  # validates_uniqueness_of :bookmarks_id, scope: [:movie_id, :list_id]
+  validates :movie, uniqueness: { scope: :list }
 end
